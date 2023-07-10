c14161a7798d40e80e743129a96e69b4e6a244de
YaelZamora <ajedrezkasparov@gmail.com>
2023-07-09 18:33:43 -0600

    Initial Commit

---
+++ Yun/YunApp.swift
@@ -0,0 +1,17 @@
+//
+//  YunApp.swift
+//  Yun
+//
+//  Created by Yael Javier Zamora Moreno on 09/07/23.
+//
+
+import SwiftUI
+
+@main
+struct YunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
